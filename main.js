0ef0624f894d40adf946f601cf0a0d3fe3f1455e
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-14 16:47:35 +0300

    addForm

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-import './style.css'
+"use strick";
+import "./styles/main.scss";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function GetUserInfo({ addForm, userForm, addInfo }) {
+  const addBtnForm = document.querySelector(addForm);
+  const formUser = document.querySelector(userForm);
+  const addBtnInfo = document.querySelector(addInfo)
+
+  formUser.addEventListener('submit' ,(event)=>{
+    event.preventDefault()
+  })
+  
+  this.openForm = function () {
+    formUser.classList.add("open-form");
+  };
+  addBtnForm.addEventListener("click", this.openForm);
+
+  this.infoAdd = function(){
+    formUser.classList.remove("open-form");
+  }
+  addBtnInfo .addEventListener('click', this.infoAdd)
+}
+
+
+const UserInfo = new GetUserInfo({
+  userForm: ".js--form",
+  addForm: ".js--add-form",
+  addInfo: ".js--add-info"
+});
